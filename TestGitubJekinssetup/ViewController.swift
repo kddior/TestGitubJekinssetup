ce79b06dce669821688ee1eabba5b119bd0a6abd
Serge Kone Dossongui <SergeKDossongui@gmail.com>
2020-02-10 20:22:13 -0500

    first commit

---
+++ TestGitubJekinssetup/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestGitubJekinssetup
+//
+//  Created by serge kone Dossongui on 2/10/20.
+//  Copyright © 2020 serge kone Dossongui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
